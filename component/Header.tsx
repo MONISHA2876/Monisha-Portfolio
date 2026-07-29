0fe489a62d46ab389ff78ae22dd6ced33e501a2c
Monisha <monisha.singh06@gmail.com>
2026-07-29 23:48:57 +0530

    feat(navbar): added button to view resume

--- component/Header.tsx
+++ component/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "lucide-react";
 import Image from "next/image";
 
 
@@ -12,10 +13,23 @@ export default function Header() {
                 <h1 className="text-xl">Monisha Singh</h1>
             </div>
             <nav>
-                <ul className="flex space-x-4">
+                <ul className="flex space-x-6 items-center">
                     <li><a href="/" className="hover:underline">Home</a></li>
                     <li><a href="/about" className="hover:underline">About</a></li>
+                    <li><a href="/projects" className="hover:underline">Projects</a></li>
+                    <li><a href="/experience" className="hover:underline">Experience</a></li>
                     <li><a href="/contact" className="hover:underline">Contact</a></li>
+
+                    
+                    <a
+                    href="/resume/Monisha_Singh_Resume.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="rounded-full border border-neutral-700 px-5 py-2 text-['15px'] transition hover:bg-white hover:text-black"
+                    >
+                    Resume ↗
+                    </a>
+                    
                 </ul>
             </nav>
         </div>
